8cae98a4e7c3d588851116087e8153b2b1e8769c
Nels885 <voirin.lionel@gmail.com>
2018-06-05 15:47:08 +0200

    creation of the HTML page with bootstrap

--- bot/tests/test_grandpy_bot.py
+++ bot/tests/test_grandpy_bot.py
@@ -1,2 +1,16 @@
-import pytest
+import urllib
+from flask_testing import LiveServerTestCase
 
+from .. import app
+
+
+class TestGrand(LiveServerTestCase):
+
+    def create_app(self):
+        app.config.from_object('bot.tests.config')
+        return app
+
+# test si le serveur est bien démarré.
+    def test_server_is_up_and_running(self):
+        response = urllib.request.urlopen(self.get_server_url())
+        self.assertEqual(response.code, 200)
