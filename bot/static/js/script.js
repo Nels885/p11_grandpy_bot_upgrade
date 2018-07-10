a8c01d3cf573f3b951191d60e31cdef391a17b86
Nels885 <voirin.lionel@gmail.com>
2018-07-10 12:43:42 +0200

    Added animation for loading and editing AJAX requests for JQuery 3.3.1

--- bot/static/js/script.js
+++ bot/static/js/script.js
@@ -1,38 +1,42 @@
-$(function(){
-    $('form').on('submit', function(e) {
+$(function () {
+    $('form').on('submit', function (e) {
         e.preventDefault();
+        const chat = $('#chat');
 
         // hide the message "Posez votre question à GrandPy Bot !"
-        $('#msgPres').hide();
+        // $('#msgPres').hide();
 
         // Adding the user message in the chat window
-        var msgUser = $('#content').val();
-        $('#chat').append('<div class="cont-chat darker text-right">' +
+        const msgUser = $('#content').val();
+        chat.append('<div class="cont-chat darker text-right">' +
             '<img class="right" src="../static/img/invite.png" alt="Avatar">' +
             '<p>' + msgUser + '</p></div>');
 
+        // Adding load animation
+        chat.append('<div class="cont-chat text-center" id="load">' +
+            '<div id="loader"></div></div>');
+
         // AJAX requests to the post and displays the PapyBot message according to the user message
         $.ajax({
-            url: '/post/',
-            data: $('form').serialize(),
-            type: 'POST',
-            success: function(response){
+            method: 'POST',
+            url: '/',
+            data: {content: msgUser},
+        }).done(function(response) {
 
-                // Display the messages of GrandyBot
-                var obj = JSON.parse(response);
-                answersList = obj.message;
-                console.log(obj.message);
-                answersList.forEach(function(answer) {
-                    $('#chat').append('<div class="cont-chat">' +
-                        '<img class="left" src="../static/img/papybot.png" alt="Avatar">' +
-                        '<p>' + answer + '</p></div>');
-                });
-                },
-            error: function(error){
+            // Remove load animation
+            $('#load').remove();
 
-                // Display error message in the console
-                console.log(error);
-            }
+            // Display the messages of GrandyBot
+            const answersList = JSON.parse(response).message;
+            answersList.forEach(function (answer) {
+                $('#chat').append('<div class="cont-chat">' +
+                    '<img class="left" src="../static/img/papybot.png" alt="Avatar">' +
+                    '<p>' + answer + '</p></div>');
+            });
+        }).fail(function(error) {
+
+            // Display error message in the console
+            console.error(error);
         });
     });
 });
--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,4 @@
+import time
 from .models import *
 from flask import Flask, render_template, request, json
 
@@ -17,13 +18,18 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/post/', methods=['POST'])
+@app.route('/', methods=['POST'])
 def addpost():
     """
     Receives user messages and return GranpyBot answers
     :return: return the answers of GrandpyBot in JSON format
     """
     message = request.form['content']
+
+    # load animation test
+    time.sleep(5)
+
+    # returns the message of GrandPy Bot in JSON format
     msgBot = ["Je ne comprends pas la question !!"]
     for msg, answers in answerBot.items():
         if message == msg:
